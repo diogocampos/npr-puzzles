3ea54b9702ce24c8af73f7aad624446769408e98
Diogo Campos <diogocampos@users.noreply.github.com>
2018-08-02 13:10:10 -0300

    Refactor utils

--- util.py
+++ util.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 
+WORDLISTS_DIR = Path(__file__).parent / 'wordlists'
+
+
+def wordlist(filename):
+    return read_words(WORDLISTS_DIR / filename)
+
 
 def all_wordlists():
-    dir = Path(__file__).parent / 'wordlists'
-    for filename in dir.glob('*.txt'):
-        with open(filename, 'r') as file:
-            yield from wordlist(file)
+    for path in WORDLISTS_DIR.glob('*.txt'):
+        yield from read_words(path)
 
 
-def wordlist(file):
-    for line in file:
-        for word in line.split():
-            if word.isalpha(): yield word.lower()
+def read_words(path):
+    with open(path, 'r') as file:
+        for line in file:
+            for word in line.split(): yield word.lower()
