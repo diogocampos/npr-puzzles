9e17d1637efe7ebec62a9955d68cddf7af85ac25
Diogo Campos <diogocampos@users.noreply.github.com>
2018-08-06 14:01:00 -0300

    Add script template

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+NPR Sunday Puzzle, {{day}} {{month}} {{year}}
+
+{{description}}
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Solution: {{solution}}
